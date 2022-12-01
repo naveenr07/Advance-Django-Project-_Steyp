af896687f4fb0e22f6a2af2e4beaf9dc1bfacb89
naveenr07 <naveenrs651@gmail.com>
2022-12-01 12:54:06 +0530

    uploaded data view section completed

--- web/forms.py
+++ web/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class FileUploadForm(forms.Form):
     file_name = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter file name'}))
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your file name'}))
     files_data = forms.FileField(widget=forms.FileInput(
         attrs={'accept': '.csv'}))
